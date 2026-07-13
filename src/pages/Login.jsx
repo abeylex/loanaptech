1e4980aad5708965f2f01fb8c66d6b55717e569c
abeylex <abeylex1501@gmail.com>
2026-07-13 15:11:15 +0100

    new

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://loanaptech-piyi.onrender.com/api/auth/login", {
+      const response = await fetch("https://loanaptech-ijz6.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
