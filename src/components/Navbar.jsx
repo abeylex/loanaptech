0a961a77d86a817d25f32c60e810f2f9d5a6e365
abeylex <abeylex1501@gmail.com>
2026-07-15 13:39:48 +0100

    others

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
   const checkAuth = async () => {
     try {
       const response = await fetch(
-        "https://loanaptech-ijz6.onrender.com/api/auth/me",
+        "https://loanaptech-piyi.onrender.com/api/auth/me",
         { credentials: "include" }
       );
       if (response.ok) {
@@ -36,7 +36,7 @@ function Navbar() {
 
   const handleLogout = async () => {
   try {
-    const res = await fetch("https://loanaptech-ijz6.onrender.com/api/auth/logout", {
+    const res = await fetch("https://loanaptech-piyi.onrender.com/api/auth/logout", {
       method: "POST",
       credentials: "include",
     });
