46f9db81e68f725c500fec142b4eaa6f3b514f0f
abeylex <abeylex1501@gmail.com>
2026-07-17 13:06:35 +0100

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Login from "./pages/Login";
-//import Apply from "./pages/Apply";
+import Apply from "./pages/Apply";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
@@ -23,7 +23,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/apply" element={<Apply />} />
         
          <Route path="/signup" element={<Signup />} />
          <Route path="/dashboard" element={<Dashboard />} />
